bcff17bca9313578a21ba4f27f9a2d462303dad1
yairklo <yair.klausner@mail.huji.ac.il>
2025-11-27 12:50:34 +0200

    eslintrc

--- next_app/next.config.ts
+++ next_app/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Allow production builds to successfully complete even if
+    // there are ESLint errors. We still see them in logs.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // If type errors appear in CI, you can temporarily allow builds to proceed.
+    // Ideally fix types and set this back to false.
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
