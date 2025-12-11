fb8c2d5ca0d9556c8350532bcee37fae1809e70c
yairklo <yair.klausner@mail.huji.ac.il>
2025-12-11 20:28:46 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,9 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      'react/no-unescaped-entities': 'off' 
+    },
   },
 ])
