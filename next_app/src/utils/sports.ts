9949e4823ba7ebe2d74491868d5ed8a539599a9c
yairklo <yair.klausner@mail.huji.ac.il>
2026-01-04 14:19:49 +0200

    Update sports.ts

--- next_app/src/utils/sports.ts
+++ next_app/src/utils/sports.ts
@@ -1,7 +1,7 @@
 export const SPORT_IMAGES = {
-    SOCCER: "https://images.unsplash.com/photo-1517927033932-b3d18e61fb3a?auto=format&fit=crop&w=800&q=80",
-    BASKETBALL: "https://images.unsplash.com/photo-1504450758481-7338eba7524a?auto=format&fit=crop&w=800&q=80",
-    TENNIS: "https://images.unsplash.com/photo-1595435934249-5df7ed86c1c0?auto=format&fit=crop&w=800&q=80"
+    SOCCER: "https://images.unsplash.com/photo-1579952363873-27f3bde9be2d?auto=format&fit=crop&w=800&q=80",
+    BASKETBALL: "https://images.unsplash.com/photo-1519861531473-920026393112?auto=format&fit=crop&w=800&q=80",
+    TENNIS: "https://images.unsplash.com/photo-1554068865-24cecd4e34b8?auto=format&fit=crop&w=800&q=80"
 };
 
 export const SPORT_MAPPING: Record<string, string> = {
