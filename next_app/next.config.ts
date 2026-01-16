cefabcab2483ba006aa3207c893d856c075e6358
yairklo <yair.klausner@mail.huji.ac.il>
2026-01-16 13:13:42 +0200

    fixed

--- next_app/next.config.ts
+++ next_app/next.config.ts
@@ -1,4 +1,12 @@
 import type { NextConfig } from "next";
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
   eslint: {
@@ -13,4 +21,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig as any);
