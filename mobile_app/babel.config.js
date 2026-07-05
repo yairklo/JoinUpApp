abb564688ff1bba98d84587a24e012624fbfa00c
yairklo <yair.klausner@mail.huji.ac.il>
2026-07-05 16:34:17 +0300

    fix: resolve expo-updates and expo-localization versions for SDK 54 and fix babel reanimated plugin

--- mobile_app/babel.config.js
+++ mobile_app/babel.config.js
@@ -5,6 +5,6 @@ module.exports = function (api) {
             ["babel-preset-expo", { jsxImportSource: "nativewind" }],
             "nativewind/babel",
         ],
-        plugins: ["react-native-worklets/plugin"],
+        plugins: ["react-native-reanimated/plugin"],
     };
 };
